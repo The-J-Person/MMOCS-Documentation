4d67272fc03f5888106d11499ab154e620494308
Gregorius Maximus <rogovin_grisha@hotmail.com>
2015-06-01 19:21:10 +0300

    Added remove item from inventory in DB

--- MySql Generated Scripts/SQL.sql
+++ MySql Generated Scripts/SQL.sql
@@ -704,6 +704,46 @@ DELIMITER ;
 /*!50003 SET character_set_client  = @saved_cs_client */ ;
 /*!50003 SET character_set_results = @saved_cs_results */ ;
 /*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `Remove_Item_From_Inventory` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `Remove_Item_From_Inventory`(
+		IN PlayerID INTEGER, IN ResourecID INTEGER, IN Amnt INTEGER, OUT Result INTEGER)
+BEGIN
+	IF(SELECT COUNT(*) FROM user WHERE UserID = UID) = 0
+    THEN
+		SET Result = 0;
+	ELSEIF(SELECT COUNT(*) FROM charinv WHERE UserID = UID AND RID = ResourecID) = 0
+    THEN
+		SET Result = 1;
+	ELSE 
+		SET @NUM := (SELECT Amount FROM charinv WHERE UserID = UID AND RID = ResourecID);
+		IF(Amnt > NUM) 
+        THEN
+			SET Result = 2;
+		ELSE
+			SET Result = 3;
+			IF(NNUM = Amnt)
+            THEN
+				DELETE FROM charinv WHERE UserID = UID AND RID = ResourecID;
+			ELSE
+				UPDATE charinv SET Amount = Amount - Amnt WHERE UserID = PlayerID AND RID = ResourecID; 
+			END IF;
+		END IF;
+	END IF;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
 /*!50003 DROP PROCEDURE IF EXISTS `Remove_Monster` */;
 /*!50003 SET @saved_cs_client      = @@character_set_client */ ;
 /*!50003 SET @saved_cs_results     = @@character_set_results */ ;
@@ -823,4 +863,4 @@ DELIMITER ;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2015-06-01 18:21:59
+-- Dump completed on 2015-06-01 19:18:55
